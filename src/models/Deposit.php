4a8405d73d4d825cc3fbadd20dfb8eb33e63c4b1
kennCK <kennettecanales@gmail.com>
2020-09-30 17:07:17 +0800

    update

--- src/models/Deposit.php
+++ src/models/Deposit.php
@@ -5,7 +5,7 @@ namespace Increment\Finance\Models;
 use Illuminate\Database\Eloquent\Model;
 use App\APIModel;
 
-class DepositPayment extends APIModel
+class Deposit extends APIModel
 {
     protected $table = 'deposits';
     protected $fillable = ['code', 'account_id', 'account_code', 'currency', 'amount', 'payload', 'payload_value'];
