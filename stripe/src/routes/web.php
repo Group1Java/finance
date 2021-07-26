da5c81f93339b625849521fd2aeea86212501763
= <=>
2021-07-27 00:15:03 +0800

    udpate

--- stripe/src/routes/web.php
+++ stripe/src/routes/web.php
@@ -4,6 +4,6 @@
 // Charges Controller
 $route = env('PACKAGE_ROUTE', '').'/stripe_webhooks/';
 $controller = 'Increment\Finance\Stripe\Http\StripeController@';
-Route::post($route.'retrieve', $controller."retrieve");
+Route::post($route.'charge_customer', $controller."chargeCustomer");
 Route::get($route.'test', $controller."test");
 
